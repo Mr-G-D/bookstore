868ed46df8fc6633890d6677c91f18af5459a47e
Mr-G-D <daredinesh00@gmail.com>
2024-03-15 18:59:31 -0400

    Book class :pencil2:

--- src/bookkeeper/Book.java
+++ src/bookkeeper/Book.java
@@ -1,20 +1,25 @@
 package bookkeeper;
 
+import java.io.Serializable;
+
 /**
  * @author Sanjay Bharathi
+ * @author DINESH KUMAR
  */
 
-public class Book {
+public class Book implements Serializable {
 
-    String title, authors, price, isbn, genre, year;
+    String title, authors, isbn, genre;
+    double price;
+    int year;
 
     public Book(String[] bookDetails) {
         this.title = bookDetails[0];
         this.authors = bookDetails[1];
-        this.price = bookDetails[2];
+        this.price = Double.parseDouble(bookDetails[2]);
         this.isbn = bookDetails[3];
         this.genre = bookDetails[4];
-        this.year = bookDetails[5];
+        this.year = Integer.parseInt(bookDetails[5]);
     }
 
 }
