b42d987eac8f7b9f1c8cd03dc0e0d5a4890e2fa5
Mr-G-D <daredinesh00@gmail.com>
2024-03-24 09:17:19 -0400

    conflicts  :bug:

--- src/bookkeeper/exceptions/SemanticErrorException.java
+++ src/bookkeeper/exceptions/SemanticErrorException.java
@@ -1,15 +1,20 @@
 package bookkeeper.exceptions;
 
 /**
- * Parent exception for all Semantic Errors
+ * @author Sanjay Bharathi
  * @author DINESH KUMAR
- *
  * Assignment 2
  * Written by: Dinesh Kumar Gopinathan - 40273281, Sanjay Bharathi Subramanian - 40248572
  * 24 March 2024
+ *
+ * This class is used to handle the semantic errors
  */
 public class SemanticErrorException extends Exception{
 
+    /**
+     * Constructor to initialize the exception
+     * @param s - message to be displayed
+     */
     public SemanticErrorException(String s) {
         super(s);
     }
--- src/bookkeeper/exceptions/SyntaxErrorException.java
+++ src/bookkeeper/exceptions/SyntaxErrorException.java
@@ -1,19 +1,19 @@
 package bookkeeper.exceptions;
-
 /**
  * @author Sanjay Bharathi
- * Parent exception that represents all Syntax Exceptions
- *
+ * @author DINESH KUMAR
  * Assignment 2
  * Written by: Dinesh Kumar Gopinathan - 40273281, Sanjay Bharathi Subramanian - 40248572
  * 24 March 2024
+ *
+ * This class is used to handle the syntax errors
  */
 
 public class SyntaxErrorException extends Exception{
 
     /**
-     * Constructor
-     * @param s Exception message
+     * Constructor to initialize the exception
+     * @param s - message to be displayed
      */
     public SyntaxErrorException(String s) {
         super(s);
