1329b67ad996f904ba662d7b87831e7c6003d24d
Mr-G-D <daredinesh00@gmail.com>
2024-03-24 09:17:54 -0400

    conflicts  :bug:

--- src/bookkeeper/exceptions/semantic/BadIsbn10Exception.java
+++ src/bookkeeper/exceptions/semantic/BadIsbn10Exception.java
@@ -1,18 +1,19 @@
 package bookkeeper.exceptions.semantic;
 
 import bookkeeper.exceptions.SemanticErrorException;
-
 /**
- * This exception is thrown when the ISBN 10 is invalid
+ * @author Sanjay Bharathi
  * @author DINESH KUMAR
  * Assignment 2
  * Written by: Dinesh Kumar Gopinathan - 40273281, Sanjay Bharathi Subramanian - 40248572
  * 24 March 2024
  *
+ * This class is used to handle the semantic errors
  */
 public class BadIsbn10Exception extends SemanticErrorException {
+
     /**
-     * Constructor
+     * Constructor to initialize the exception
      */
     public BadIsbn10Exception(){
         super("invalid isbn10");
